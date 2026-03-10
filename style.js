da5295c929e2fe440477018720e85f206cc30244
abdo ayed <aayed0377@gmail.com>
2026-03-10 14:19:11 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container")
+
+function phone (x){
+    img.src =x ;
+}
+
+function back(color){
+    container.style.background = color;
+}
